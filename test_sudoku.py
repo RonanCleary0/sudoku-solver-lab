c1188e581c78332bf32fbc6f481c5958bb7d013b
Ronan <c22382501@mytudublin.ie>
2024-12-02 23:37:29 +0000

    Initial commit with Sudoku solver and tests

---
+++ test_sudoku.py
@@ -0,0 +1,38 @@
+import unittest
+from sudoku import solve_sudoku
+
+class TestSudokuSolver(unittest.TestCase):
+    def test_solve_valid_sudoku(self):
+        board = [
+            [5, 3, 0, 0, 7, 0, 0, 0, 0],
+            [6, 0, 0, 1, 9, 5, 0, 0, 0],
+            [0, 9, 8, 0, 0, 0, 0, 6, 0],
+            [8, 0, 0, 0, 6, 0, 0, 0, 3],
+            [4, 0, 0, 8, 0, 3, 0, 0, 1],
+            [7, 0, 0, 0, 2, 0, 0, 0, 6],
+            [0, 6, 0, 0, 0, 0, 2, 8, 0],
+            [0, 0, 0, 4, 1, 9, 0, 0, 5],
+            [0, 0, 0, 0, 8, 0, 0, 7, 9]
+        ]
+        solution = solve_sudoku(board)
+        self.assertIsNotNone(solution)
+        self.assertEqual(len(solution), 9)
+
+    def test_solve_invalid_sudoku(self):
+        board = [
+            [5, 3, 0, 0, 7, 0, 0, 0, 0],
+            [6, 0, 0, 1, 9, 5, 0, 0, 0],
+            [0, 9, 8, 0, 0, 0, 0, 6, 0],
+            [8, 0, 0, 0, 6, 0, 0, 0, 3],
+            [4, 0, 0, 8, 0, 3, 0, 0, 1],
+            [7, 0, 0, 0, 2, 0, 0, 0, 6],
+            [0, 6, 0, 0, 0, 0, 2, 8, 0],
+            [0, 0, 0, 4, 1, 9, 0, 0, 5],
+            [0, 0, 0, 0, 8, 0, 0, 7, 8]
+        ]
+        solution = solve_sudoku(board)
+        self.assertIsNone(solution)
+
+if __name__ == '__main__':
+    unittest.main()
+
